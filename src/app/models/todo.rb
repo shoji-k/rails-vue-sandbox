cf0c34e4c2eef9ffdc9484d7b6e55f9b742faaf6
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2019-11-28 13:04:58 +0900

    install annotate and setup

--- src/app/models/todo.rb
+++ src/app/models/todo.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id         :integer          not null, primary key
+#  detail     :text
+#  done       :boolean          default(FALSE), not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Todo < ApplicationRecord
 end
--- src/spec/models/todo_spec.rb
+++ src/spec/models/todo_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  detail     :text
+#  done       :boolean          default(FALSE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
