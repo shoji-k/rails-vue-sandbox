375d2aabbf974fdf56f5b97138dcd4474913d1c2
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-07-16 14:36:01 +0900

    add sample job

---
+++ src/app/jobs/sample_job.rb
@@ -0,0 +1,20 @@
+require 'net/http'
+require 'uri'
+
+class SampleJob < ApplicationJob
+  queue_as :default
+  after_perform :after_perform
+
+  def perform(*args)
+    uri = URI.parse("https://httpbin.org/ip")
+    response = Net::HTTP.get_response(uri)
+    response.code
+    # p response.body
+  end
+
+  private
+
+  def after_perform
+    p 'finished'
+  end
+end
