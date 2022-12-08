9dbdc2952c70a8d228eb20309809bd616fa05131
shoji kobayashi <shoji.kobayashi.0125@gmail.com>
2022-12-08 15:16:40 +0900

    update ruby and rails

--- src/spec/rails_helper.rb
+++ src/spec/rails_helper.rb
@@ -85,7 +85,7 @@ RSpec.configure do |config|
   end
 
   def committee_options
-    @committee_options ||= { schema_path: schema_path, prefix: '/api/v1', old_assert_behavior: true, parse_response_by_content_type: false,
+    @committee_options ||= { schema_path:, prefix: '/api/v1', old_assert_behavior: true, parse_response_by_content_type: false,
                              query_hash_key: 'rack.request.query_hash' }
   end
 end
