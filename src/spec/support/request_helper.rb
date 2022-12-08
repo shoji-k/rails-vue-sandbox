b6a02dbcc5578808f584bdc92ea03e90e584838e
shoji kobayashi <shoji.kobayashi.0125@gmail.com>
2022-12-08 16:43:34 +0900

    rails 7 settings

--- src/spec/support/request_helper.rb
+++ src/spec/support/request_helper.rb
@@ -1,6 +1,6 @@
 module RequestHelper
   def authenticated_header(user)
-    token = Knock::AuthToken.new({ payload: user.to_token_payload }).token
+    token = Knock::AuthToken.new(payload: user.to_token_payload).token
     { Authorization: "Bearer #{token}" }
   end
 end
