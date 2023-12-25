de452f33798ebb076968df6c2b87e5755dd5b52b
shoji kobayashi <shoji.kobayashi.0125@gmail.com>
2023-12-25 16:56:40 +0900

    update gems and fix deprecation warnings

--- src/spec/rails_helper.rb
+++ src/spec/rails_helper.rb
@@ -38,7 +38,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = Rails.root.join('spec/fixtures')
+  config.fixture_paths = [Rails.root.join('spec/fixtures')]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -86,7 +86,7 @@ RSpec.configure do |config|
 
   def committee_options
     @committee_options ||= { schema_path:, prefix: '/api/v1', old_assert_behavior: true, parse_response_by_content_type: false,
-                             query_hash_key: 'rack.request.query_hash' }
+                             query_hash_key: 'rack.request.query_hash', strict_reference_validation: true }
   end
 end
 
